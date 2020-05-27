631fd7bbd35f276784b43fbaf8a0f24ae5c71046
dorinzaharia <dorin.zahariazd@gmail.com>
2020-05-27 18:33:01 +0300

    Add search route to server
    Add Bing Web search to server. Request includes query arguments, which have default values if not included.
    Subscription Key is as an environment variable.
    Add eslintrc with airbnb config

--- index.js
+++ index.js
@@ -1,7 +1,52 @@
 const express = require("express");
-const app = express();
-const port = process.env.PORT || 3000;
+const https = require("https");
+const url = require("url");
+require("dotenv").config();
 
-app.listen(port, () =>
-  console.log(`App listening at http://localhost:${port}`)
-);
+const app = express();
+
+const port = process.env.PORT || 3000;
+const SUBSCRIPTION_KEY = process.env.AZURE_SUBSCRIPTION_KEY;
+if (!SUBSCRIPTION_KEY) {
+  throw new Error("Missing the AZURE_SUBSCRIPTION_KEY environment variable");
+}
+
+app.get("/search", (req, res) => {
+  const requestUrl = url.parse(
+    url.format({
+      protocol: "https",
+      hostname: "api.cognitive.microsoft.com",
+      pathname: "/bing/v7.0/search",
+      query: {
+        q: req.query.q,
+        count: req.query.count || 10,
+        offset: req.query.offset || 0,
+        mkt: req.query.mkt || "en-US",
+      },
+    })
+  );
+  https.get(
+    {
+      hostname: requestUrl.hostname,
+      path: requestUrl.path,
+      headers: { "Ocp-Apim-Subscription-Key": SUBSCRIPTION_KEY },
+    },
+
+    (response) => {
+      let body = [];
+      response
+        .on("data", (responseData) => {
+          body += responseData;
+        })
+        .on("end", () => {
+          const data = JSON.parse(body);
+          res.status(200).json(data);
+        })
+        .on("error", (error) => {
+          res.status(500).json({ error });
+        });
+    }
+  );
+});
+
+app.listen(port);
