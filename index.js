e5880ba1b2297683344f0414b65b0e80fd29e70e
dorinzaharia <dorin.zahariazd@gmail.com>
2020-07-23 14:25:32 +0300

    Use middleware for routing

--- index.js
+++ index.js
@@ -1,100 +1,20 @@
 const express = require("express");
-const https = require("https");
-const url = require("url");
 const cors = require("cors");
+const mongoose = require("mongoose");
 
 const {
-  port,
-  bingHostname,
-  bingWebSearchPathname,
-  bingWebSearchKey,
-  bingCustomSearchPathname,
-  bingCustomSearchKey,
-  bingCustomSearchCustomConfig,
+  port
 } = require("./config");
 
 const app = express();
 app.use(cors());
 
-app.get("/search", (req, res) => {
-  const requestUrl = url.parse(
-    url.format({
-      protocol: "https",
-      hostname: bingHostname,
-      pathname: bingWebSearchPathname,
-      query: {
-        q: req.query.q,
-        count: req.query.count || 10,
-        offset: req.query.offset || 0,
-        mkt: req.query.mkt || "en-US",
-      },
-    })
-  );
-  https.get(
-    {
-      hostname: requestUrl.hostname,
-      path: requestUrl.path,
-      headers: { "Ocp-Apim-Subscription-Key": bingWebSearchKey },
-    },
+const searchRoutes = require("./routes/webSearch");
 
-    (response) => {
-      let body = [];
-      response
-        .on("data", (responseData) => {
-          body += responseData;
-        })
-        .on("end", () => {
-          const data = JSON.parse(body);
-          res.status(200).json(data);
-        })
-        .on("error", (error) => {
-          res.status(500).json({ error });
-        });
-    }
-  );
-});
-
-app.get("/customsearch", (req, res) => {
-  const requestUrl = url.parse(
-    url.format({
-      protocol: "https",
-      hostname: bingHostname,
-      pathname: bingCustomSearchPathname,
-      query: {
-        q: req.query.q,
-        customconfig: req.query.customconfig || bingCustomSearchCustomConfig,
-        count: req.query.count || 10,
-        offset: req.query.offset || 0,
-        mkt: req.query.mkt || "en-US",
-      },
-    })
-  );
-  https.get(
-    {
-      hostname: requestUrl.hostname,
-      path: requestUrl.path,
-      headers: { "Ocp-Apim-Subscription-Key": bingCustomSearchKey },
-    },
-
-    (response) => {
-      let body = [];
-      response
-        .on("data", (responseData) => {
-          body += responseData;
-        })
-        .on("end", () => {
-          const data = JSON.parse(body);
-          res.status(200).json(data);
-        })
-        .on("error", (error) => {
-          res.status(500).json({ error });
-        });
-    }
-  );
-});
+app.use("/search", searchRoutes);
 
 app.get("*", (req, res) => {
-  res.status(500).json({
+  res.status(404).json({
     message: "Route does not exist",
   });
 });
---
+++ routes/webSearch.js
@@ -0,0 +1,95 @@
+const router = require('express').Router();
+const https = require("https");
+const url = require("url");
+
+// ENV variables
+const {
+    bingHostname,
+    bingWebSearchPathname,
+    bingWebSearchKey,
+    bingCustomSearchPathname,
+    bingCustomSearchKey,
+    bingCustomSearchCustomConfig,
+  } = require("../config");
+
+// Bing Web Search route
+router.get("/web", (req, res) => {
+    const requestUrl = url.parse(
+      url.format({
+        protocol: "https",
+        hostname: bingHostname,
+        pathname: bingWebSearchPathname,
+        query: {
+          q: req.query.q,
+          count: req.query.count || 10,
+          offset: req.query.offset || 0,
+          mkt: req.query.mkt || "en-US",
+        },
+      })
+    );
+    https.get(
+      {
+        hostname: requestUrl.hostname,
+        path: requestUrl.path,
+        headers: { "Ocp-Apim-Subscription-Key": bingWebSearchKey },
+      },
+  
+      (response) => {
+        let body = [];
+        response
+          .on("data", (responseData) => {
+            body += responseData;
+          })
+          .on("end", () => {
+            const data = JSON.parse(body);
+            res.status(200).json(data);
+          })
+          .on("error", (error) => {
+            res.status(500).json({ error });
+          });
+      }
+    );
+  });
+
+
+// Bing Custom Search route
+router.get("/custom", (req, res) => {
+    const requestUrl = url.parse(
+      url.format({
+        protocol: "https",
+        hostname: bingHostname,
+        pathname: bingCustomSearchPathname,
+        query: {
+          q: req.query.q,
+          customconfig: req.query.customconfig || bingCustomSearchCustomConfig,
+          count: req.query.count || 10,
+          offset: req.query.offset || 0,
+          mkt: req.query.mkt || "en-US",
+        },
+      })
+    );
+    https.get(
+      {
+        hostname: requestUrl.hostname,
+        path: requestUrl.path,
+        headers: { "Ocp-Apim-Subscription-Key": bingCustomSearchKey },
+      },
+  
+      (response) => {
+        let body = [];
+        response
+          .on("data", (responseData) => {
+            body += responseData;
+          })
+          .on("end", () => {
+            const data = JSON.parse(body);
+            res.status(200).json(data);
+          })
+          .on("error", (error) => {
+            res.status(500).json({ error });
+          });
+      }
+    );
+  });
+
+module.exports = router;
